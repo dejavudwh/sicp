0d68e1c88c9c09bd5490bc0f8ab9aeaa024fad6d
dejavudwh <494319000@qq.com>
2019-02-28 15:20:42 +0800

    chapter 2 /2.28

---
+++ Chapter 2/exercise2.42.rkt
@@ -0,0 +1,59 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc sequence)
+  (accumulate append nil (map proc sequence)))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence)
+         nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (adjoin-position new-row rest-of-queens)
+  (cons new-row rest-of-queens))
+
+(define empty-board '())
+
+(define (safe? k position)
+  (define (iter new-queen rest-queens i)
+    (if (null? rest-queens)
+        #t
+        (let ((queen (car rest-queens)))
+          (if (or (= new-queen queen)
+                  (= new-queen (+ i queen))
+                  (= new-queen (- queen i)))
+              #f
+              (iter new-queen (cdr rest-queens) (+ i 1))))))
+  (iter (car position) (cdr position) 1))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (position) (safe? k position))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(for-each (lambda (pos)
+                    (begin
+                        (display pos)
+                        (newline)))
+                (queens 8))
+            
