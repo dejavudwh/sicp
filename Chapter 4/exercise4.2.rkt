c74aaad2c2b380cd5cb53682022ec662185f152c
dejavudwh <494319000@qq.com>
2019-03-07 18:23:17 +0800

    D:/Git/3.7

---
+++ Chapter 4/exercise4.2.rkt
@@ -0,0 +1,3 @@
+(define (application? exp) (tagged-list? exp 'call))
+(define (operator exp) (cadr exp))
+(define (operands exp) (cddr exp))
--- Chpater 3/exercise3.59.rkt
+++
@@ -1,6 +0,0 @@
-(define (integrate-series a)
-    (mul-streams a                                  
-                 (div-streams ones integers)))
-
-(define (div-streams s1 s2)
-    (stream-map / s1 s2))
