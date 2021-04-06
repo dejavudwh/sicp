74ec9242bec2806feaca3d0f822fbb81ba3808d8
dejavudwh <494319000@qq.com>
2021-04-06 20:16:05 +0800

    complete constraint system

--- py/constraint.py
+++ py/constraint.py
@@ -1,9 +1,13 @@
+from operator import mul, truediv, add, sub
+
+
 def make_connector(name=None):
     """A connector between constraints."""
     informant = None
     constraints = []
 
     def set_value(source, value):
+        nonlocal informant
         val = connector['val']
         if val is None:
             informant, connector['val'] = source, value
@@ -15,6 +19,7 @@ def make_connector(name=None):
                 print('Contradiction detected:', val, 'vs', value)
 
     def forget_value(source):
+        nonlocal informant
         if informant == source:
             informant, connector['val'] = None, None
             if name is not None:
@@ -37,3 +42,63 @@ def inform_all_except(source, message, constraints):
     for c in constraints:
         if c != source:
             c[message]()
+
+
+def make_ternary_constraint(a, b, c, ab, ca, cb):
+    """The constraint that ab(a,b)=c and ca(c,a)=b and cb(c,b) = a."""
+    def new_value():
+        av, bv, cv = [connector['has_val']() for connector in (a, b, c)]
+        if av and bv:
+            c['set_val'](constraint, ab(a['val'], b['val']))
+        elif av and cv:
+            b['set_val'](constraint, ca(c['val'], a['val']))
+        elif bv and cv:
+            a['set_val'](constraint, cb(c['val'], b['val']))
+
+    def forget_value():
+        for connector in (a, b, c):
+            connector['forget'](constraint)
+
+    constraint = {'new_val': new_value, 'forget': forget_value}
+    for connector in (a, b, c):
+        connector['connect'](constraint)
+
+    return constraint
+
+
+def constant(connector, value):
+    """The constraint that connector = value."""
+    constraint = {}
+    connector['set_val'](constraint, value)
+
+    return constraint
+
+
+def multiplier(a, b, c):
+    """The constraint that a * b = c."""
+    return make_ternary_constraint(a, b, c, mul, truediv, truediv)
+
+
+def adder(a, b, c):
+    """The constraint that a + b = c."""
+    return make_ternary_constraint(a, b, c, add, sub, sub)
+
+
+def make_converter(c, f):
+    """Connect c to f with constraints to convert from Celsius to Fahrenheit."""
+    u, v, w, x, y = [make_connector() for _ in range(5)]
+    multiplier(c, w, u)
+    multiplier(v, x, u)
+    adder(v, y, f)
+    constant(w, 9)
+    constant(x, 5)
+    constant(y, 32)
+
+
+if __name__ == '__main__':
+    celsius = make_connector('Celsius')
+    fahrenheit = make_connector('Fahrenheit')
+    celsius['set_value']('user', 25)
+    fahrenheit['set_value']('user', 212)
+    celsius['forget']('user')
+    fahrenheit['set_value']('user', 212)
