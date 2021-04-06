b58b730790feba4ca7f7606f1935fc3f68bc16c5
dejavudwh <494319000@qq.com>
2021-04-06 17:35:57 +0800

    update make-connector

---
+++ py/constraint.py
@@ -0,0 +1,36 @@
+def make_connector(name=None):
+    """A connector between constraints."""
+    informant = None
+    constraints = []
+
+    def set_value(source, value):
+        val = connector['val']
+        if val is None:
+            informant, connector['val'] = source, value
+            if name is not None: 
+                print(name, '=', value)
+            inform_all_except(source, 'new_val', constraints)
+        else:
+            if val != value:
+                print('Contradiction detected:', val, 'vs', value)
+
+    def forget_value(source):
+        if informant == source:
+            informant, connector['val'] = None, None
+            if name is not None:
+                print(name, 'is forgotten')
+            inform_all_except(source, 'forget', constraints)
+
+    connector = {
+        'val': None,
+        'set_value': set_value,
+        'forget': forget_value,
+        'has_value': lambda: connector['val'] is not None,
+        'connect': lambda source: constraints.append(source)
+    }
+
+    return connector
+
+
+def inform_all_except():
+    pass
